adfd7be9a66b88c582619061f6778a911643bd39
Yogiraj Lokare <yhlokare1962@gmail.com>
2021-11-10 04:05:35 +0530

    updated bookings page

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,38 +1,32 @@
-import React, { Component } from 'react';
-import '../sass/Booking.css';
+import axios from "axios";
+import React, { Component, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../sass/Booking.css";
 
-export default class Bookings extends Component {
-    render() {
-        return (
-            <div>
-                <div class="hero">
-
-                    <div class="card">
-                        <h3>B-001</h3>
-                        <h4>Hotel Name: Paradise Stay</h4>
-                        <h4>Start Date:1/1/2020</h4>
-                        <h4>End Date:1/1/2020</h4>
-                        <h4>Number of person</h4>
-                        <button>Cancel</button>
-                    </div>
-                    <div class="card">
-                        <h3>B-001</h3>
-                        <h4>Hotel Name: Paradise Stay</h4>
-                        <h4>Start Date:1/1/2020</h4>
-                        <h4>End Date:1/1/2020</h4>
-                        <h4>Number of person</h4>
-                        <button>Cancel</button>
-                    </div>
-                    <div class="card">
-                        <h3>B-001</h3>
-                        <h4>Hotel Name: Paradise Stay</h4>
-                        <h4>Start Date:1/1/2020</h4>
-                        <h4>End Date:1/1/2020</h4>
-                        <h4>Number of person</h4>
-                        <button>Cancel</button>
-                    </div>
-                </div>
+const Bookings = () => {
+  const [bookings, setBookings] = useState([]);
+  useEffect(async () => {
+    const { data } = await axios.get("http://localhost:1111/bookings");
+    setBookings(data);
+  }, []);
+  return (
+    <div>
+      <div class="hero">
+        {bookings.map((booking, index) => {
+          return (
+            <div key={index} class="booking-card">
+              <h4>{booking.hotelName}</h4>
+              <h4>Start Date:{booking.startDate}</h4>
+              <h4>End Date:{booking.endDate}</h4>
+              <h4>Number of person:{booking.noOfPersons}</h4>
+              <Link to={`/reschedule/${booking.id}`}>
+                <button>Reschedule</button>
+              </Link>
             </div>
-        );
-    }
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Bookings;
