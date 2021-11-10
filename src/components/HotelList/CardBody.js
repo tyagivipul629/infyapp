15c6f7300c698a30609be7a08d8a87549d479880
Shreemauli Raut <shreemauliraut7@gmail.com>
2021-11-10 11:45:54 +0530

    Added Static Image
    improved static images on Booking Pages
    
    Co-Authored-By: Yogiraj-Lokare <68686641+Yogiraj-Lokare@users.noreply.github.com>

--- src/components/HotelList/CardBody.js
+++ src/components/HotelList/CardBody.js
@@ -6,19 +6,20 @@ export default class CardBody extends Component {
   render() {
     return (
       <div className="hotel-card">
-        <div className="logo"></div>
+        <div className="logo">
+        </div>
         <div className="card-text">
-          <h2>{this.props.hotelName}</h2>
-          <p>{this.props.city}</p>
-          <p>{this.props.amenities}</p>
-          <p>{this.props.address}</p>
-          <p>{this.props.phone}</p>
+          <h2>{ this.props.hotelName }</h2>
+          <p>{ this.props.city }</p>
+          <p>{ this.props.amenities }</p>
+          <p>{ this.props.address }</p>
+          <p>{ this.props.phone }</p>
         </div>
         <div className="card-button">
-          <Link to={`/bookRoom/${this.props.id}`}>
+          <Link to={ `/bookRoom/${this.props.id}` }>
             <button>Book Room</button>
           </Link>
-          <Link to={`/review/${this.props.id}`}>
+          <Link to={ `/review/${this.props.id}` }>
             <button>View Reviews</button>
           </Link>
         </div>
